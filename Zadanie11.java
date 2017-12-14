7b503c0529af59eddce9e9982af73a777f385df7
Astropl <30636834+Astropl@users.noreply.github.com>
2017-12-14 20:02:12 +0100

    Add files via upload

---
+++ Zadanie11.java
@@ -0,0 +1,18 @@
+public class Zadanie11
+    /*Zadanie 11. Napisać program, który pobiera od użytkownika ciąg
+            liczb całkowitych. Pobieranie danych kończone jest podaniem
+            wartości 0 (nie wliczana do danych). W następ- nej kolejności
+            program powinien wyświetlić sumę największej oraz najmniejszej z
+            podanych liczb oraz ich średnią arytmetyczną.
+            3
+            (
+            Przykład:
+            Użytkownik podał ciąg: 1, -4, 2, 17, 0. Wynik programu:
+            13 // suma min. i maks.
+            6.5 // średnia*/
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Podaj kilka liczb naturalnych. \nGdy podasz 0 oblicz sume najwiekszej \ni namniejszej liczby, i średnia arytmetyczną. ");
+    }
+}
